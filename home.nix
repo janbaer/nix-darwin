9e1dd18c76d181307843e2b6ca6bf26465ddd7cc
Jan Baer <jan@janbaer.de>
2025-04-01 15:11:07 +0200

    Define username variable to not repeat my name all the time

--- home.nix
+++ home.nix
@@ -5,10 +5,11 @@
   ...
 }: let
   inherit (config.lib.file) mkOutOfStoreSymlink;
+  username = "jan.baer";
 in {
   home.stateVersion = "24.11";
-  home.username = "jan.baer";
-  home.homeDirectory = "/Users/jan.baer";
+  home.username = "${username}";
+  home.homeDirectory = "/Users/${username}";
 
   xdg.enable = true;
 
@@ -30,22 +31,22 @@ in {
   ];
 
   home.file = {
-    ".tmux".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.tmux";
-    ".tmux.conf".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.tmux.conf";
-    ".gitattributes".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.gitattributes";
-    ".gitconfig".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.gitconfig";
-    ".gitconfig_check24".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.gitconfig_check24";
-    ".config/zsh".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/zsh";
-    ".zshenv".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.zshenv";
-    ".config/lf".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/lf";
-    ".config/lazygit".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/lazygit";
-    ".config/nvim".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/nvim";
-    ".config/Code".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/Code";
-    ".config/powerline".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/powerline";
-    ".config/atuin".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/atuin";
-    ".config/ghostty".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.config/ghostty"; 
-    ".fzf-init.zsh".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.fzf-init.zsh";
-    ".p10k.zsh".source = mkOutOfStoreSymlink "/Users/jan.baer/Projects/dotfiles/.p10k.zsh";
+    ".tmux".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.tmux";
+    ".tmux.conf".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.tmux.conf";
+    ".gitattributes".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.gitattributes";
+    ".gitconfig".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.gitconfig";
+    ".gitconfig_check24".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.gitconfig_check24";
+    ".config/zsh".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/zsh";
+    ".zshenv".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.zshenv";
+    ".config/lf".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/lf";
+    ".config/lazygit".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/lazygit";
+    ".config/nvim".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/nvim";
+    ".config/Code".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/Code";
+    ".config/powerline".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/powerline";
+    ".config/atuin".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/atuin";
+    ".config/ghostty".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.config/ghostty"; 
+    ".fzf-init.zsh".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.fzf-init.zsh";
+    ".p10k.zsh".source = mkOutOfStoreSymlink "/Users/${username}/Projects/dotfiles/.p10k.zsh";
   };
 
   programs.home-manager.enable = true;
